a2a6820a5c96f7c72d1afe2f3f1692054eedde0b
Eloi Torrents <eloitor@duck.com>
2025-04-16 23:19:47 +0200

    Add NN parent object

--- libs/algebraeon/src/lib.rs
+++ libs/algebraeon/src/lib.rs
@@ -1,16 +1,30 @@
 mod NN;
 mod ZZ;
-use koto_runtime::{Result, prelude::*};
+use koto_runtime::prelude::*;
 
 pub fn make_module() -> KMap {
-    use KValue::{Number, Str};
-
     let mut result = KMap::default();
 
-    result.add_fn("NN", |ctx| match ctx.args() {
-        [KValue::Number(n)] => Ok(NN::NN::make_koto_object(*n).into()),
-        unexpected => unexpected_args("|Number|", unexpected),
-    });
+    let mut NN = KMap::default();
+    NN.insert_meta(
+        MetaKey::Call,
+        KNativeFunction::new(|ctx| match ctx.args() {
+            [KValue::Number(n)] => Ok(NN::NN::make_koto_object(*n).into()),
+            unexpected => unexpected_args("|Number|", unexpected),
+        })
+        .into(),
+    );
+
+    NN.insert_meta(
+        MetaKey::Named("display".into()),
+        KNativeFunction::new(|_ctx| Ok("NN".into())).into(),
+    );
+
+    NN.insert_meta(
+        MetaKey::Named("primes".into()),
+        KNativeFunction::new(|_ctx| Ok("TO DO".into())).into(),
+    );
+    result.insert("NN", NN);
 
     result.add_fn("ZZ", |ctx| match ctx.args() {
         [KValue::Number(n)] => Ok(ZZ::ZZ::make_koto_object(*n).into()),

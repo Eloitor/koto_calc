2df0e31455ab9a37c52e52d3c489bb7537c96b3e
Eloi Torrents <eloitor@duck.com>
2025-04-16 13:00:38 +0200

    Add NN

---
+++ libs/algebraeon/src/NN.rs
@@ -0,0 +1,64 @@
+use koto_runtime::{Result, derive::*, prelude::*};
+
+use algebraeon::nzq::Natural;
+
+#[derive(PartialEq, Clone, KotoCopy, KotoType, Eq, Debug)]
+pub struct NN(Natural);
+
+#[koto_impl]
+impl NN {
+    pub fn make_koto_object(n: KNumber) -> KObject {
+        let my_int = Natural::from(u64::from(n));
+        KObject::from(Self(my_int))
+    }
+}
+
+impl KotoObject for NN {
+    fn add(&self, other: &KValue) -> Result<KValue> {
+        match &other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                let result = self.0.clone() + other.0.clone();
+                Ok(KValue::Object(KObject::from(Self(result))))
+            }
+            unexpected => unexpected_type("NN", unexpected),
+        }
+    }
+
+    fn display(&self, ctx: &mut DisplayContext) -> koto_runtime::Result<()> {
+        ctx.append(self.0.to_string());
+        Ok(())
+    }
+
+    fn equal(&self, other: &KValue) -> Result<bool> {
+        match &other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 == other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn multiply(&self, rhs: &KValue) -> Result<KValue> {
+        match &rhs {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                let result = self.0.clone() * other.0.clone();
+                Ok(KValue::Object(KObject::from(Self(result))))
+            }
+            unexpected => unexpected_type("NN", unexpected),
+        }
+    }
+
+    fn subtract(&self, rhs: &KValue) -> Result<KValue> {
+        match &rhs {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                let result = self.0.clone() - other.0.clone();
+                Ok(KValue::Object(KObject::from(Self(result))))
+            }
+            unexpected => unexpected_type("NN", unexpected),
+        }
+    }
+}
--- libs/algebraeon/src/lib.rs
+++ libs/algebraeon/src/lib.rs
@@ -1,3 +1,4 @@
+mod NN;
 mod ZZ;
 use koto_runtime::{Result, prelude::*};
 
@@ -5,6 +6,11 @@ pub fn make_module() -> KMap {
     use KValue::{Number, Str};
 
     let mut result = KMap::default();
+
+    result.add_fn("NN", |ctx| match ctx.args() {
+        [KValue::Number(n)] => Ok(NN::NN::make_koto_object(*n).into()),
+        unexpected => unexpected_args("|Number|", unexpected),
+    });
 
     result.add_fn("ZZ", |ctx| match ctx.args() {
         [KValue::Number(n)] => Ok(ZZ::ZZ::make_koto_object(*n).into()),

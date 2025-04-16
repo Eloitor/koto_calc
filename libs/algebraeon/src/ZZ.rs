ae820996d61fb9240cc67d13a06fbd537e0bad43
Eloi Torrents <eloitor@duck.com>
2025-04-16 11:03:08 +0200

    Add algebraeon.ZZ

---
+++ libs/algebraeon/src/ZZ.rs
@@ -0,0 +1,44 @@
+// use derive_more::From;
+// use koto::{derive::*, prelude::*, runtime};
+use koto_runtime::{Result, derive::*, prelude::*};
+
+use algebraeon::nzq::Integer;
+
+#[derive(PartialEq, Clone, KotoCopy, KotoType, Eq, Debug)]
+pub struct ZZ(Integer);
+
+#[koto_impl]
+impl ZZ {
+    pub fn make_koto_object(n: KNumber) -> KObject {
+        let my_int = Integer::from(i64::from(n));
+        KObject::from(Self(my_int))
+    }
+}
+
+impl KotoObject for ZZ {
+    fn display(&self, ctx: &mut DisplayContext) -> koto_runtime::Result<()> {
+        ctx.append(self.0.to_string());
+        Ok(())
+    }
+
+    fn add(&self, other: &KValue) -> Result<KValue> {
+        match &other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                let result = self.0.clone() + other.0.clone(); // suma de `Integer`
+                Ok(KValue::Object(KObject::from(Self(result))))
+            }
+            unexpected => unexpected_type("ZZ", unexpected),
+        }
+    }
+
+    fn equal(&self, other: &KValue) -> Result<bool> {
+        match &other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 == other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+}
--- libs/algebraeon/src/lib.rs
+++ libs/algebraeon/src/lib.rs
@@ -1,8 +1,15 @@
+mod ZZ;
 use koto_runtime::{Result, prelude::*};
 
 pub fn make_module() -> KMap {
     use KValue::{Number, Str};
 
     let mut result = KMap::default();
+
+    result.add_fn("ZZ", |ctx| match ctx.args() {
+        [KValue::Number(n)] => Ok(ZZ::ZZ::make_koto_object(*n).into()),
+        unexpected => unexpected_args("|Number|", unexpected),
+    });
+
     result
 }

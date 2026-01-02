68feac1bd93e7c1d0307afa413593195e54892f8
Eloi Torrents <eloitor@duck.com>
2026-01-02 16:11:59 +0100

    Add comparison between NN

--- libs/algebraeon/src/NN.rs
+++ libs/algebraeon/src/NN.rs
@@ -81,18 +81,25 @@ impl KotoObject for NN {
         }
     }
 
-    fn display(&self, ctx: &mut DisplayContext) -> koto_runtime::Result<()> {
-        ctx.append(self.0.to_string());
-        Ok(())
+    fn subtract(&self, rhs: &KValue) -> Result<KValue> {
+        match rhs {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                let result = self.0.clone() - other.0.clone();
+                Ok(KValue::Object(KObject::from(Self(result))))
+            }
+            unexpected => unexpected_type("NN", unexpected),
+        }
     }
 
-    fn equal(&self, other: &KValue) -> Result<bool> {
+    fn subtract_assign(&mut self, other: &KValue) -> Result<()> {
         match other {
             KValue::Object(other) if other.is_a::<Self>() => {
                 let other = other.cast::<Self>().unwrap();
-                Ok(self.0 == other.0)
+                self.0 -= other.0.clone();
+                Ok(())
             }
-            unexpected => unexpected_type("Number", unexpected),
+            unexpected => unexpected_type("NN", unexpected),
         }
     }
 
@@ -101,17 +108,6 @@ impl KotoObject for NN {
             KValue::Object(other) if other.is_a::<Self>() => {
                 let other = other.cast::<Self>().unwrap();
                 let result = self.0.clone() * other.0.clone();
-                Ok(KValue::Object(KObject::from(Self(result))))
-            }
-            unexpected => unexpected_type("NN", unexpected),
-        }
-    }
-
-    fn subtract(&self, rhs: &KValue) -> Result<KValue> {
-        match rhs {
-            KValue::Object(other) if other.is_a::<Self>() => {
-                let other = other.cast::<Self>().unwrap();
-                let result = self.0.clone() - other.0.clone();
                 Ok(KValue::Object(KObject::from(Self(result))))
             }
             unexpected => unexpected_type("NN", unexpected),
@@ -140,7 +136,58 @@ impl KotoObject for NN {
         }
     }
 
-    fn is_iterable(&self) -> IsIterable {
-        IsIterable::Iterable
+    fn equal(&self, other: &KValue) -> Result<bool> {
+        match other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 == other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn less(&self, other: &KValue) -> Result<bool> {
+        match other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 < other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn less_or_equal(&self, other: &KValue) -> Result<bool> {
+        match other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 <= other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn greater(&self, other: &KValue) -> Result<bool> {
+        match other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 > other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn greater_or_equal(&self, other: &KValue) -> Result<bool> {
+        match other {
+            KValue::Object(other) if other.is_a::<Self>() => {
+                let other = other.cast::<Self>().unwrap();
+                Ok(self.0 >= other.0)
+            }
+            unexpected => unexpected_type("Number", unexpected),
+        }
+    }
+
+    fn display(&self, ctx: &mut DisplayContext) -> koto_runtime::Result<()> {
+        ctx.append(self.0.to_string());
+        Ok(())
     }
 }
